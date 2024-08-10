747fba77b749986eb946934aecb5b851b6f612b5
anishtupe <tupeanish31@gmail.com>
2024-08-10 20:13:03 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/cuisine_decider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cuisine_decider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class welcomescreen extends StatelessWidget {
+  const welcomescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
