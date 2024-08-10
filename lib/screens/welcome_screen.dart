9aa931f2ee8b07c8975d63e10893b685334e1d66
anishtupe <tupeanish31@gmail.com>
2024-08-11 00:01:19 +0530

    made a customscaffold and made welcomescreen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cuisine_decider/widgets/custom_scaffold.dart';
 import 'package:flutter/material.dart';
 
 class welcomescreen extends StatelessWidget {
@@ -5,6 +6,38 @@ class welcomescreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return customscaffold(
+      child: Column(
+        children: [
+          Flexible(
+            child: Container(
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                        text: 'Cuisine\n Decider\n',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 45.0,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    TextSpan(
+                      text:
+                          'Welcome back! Enter personal details to register \n',
+                      style: TextStyle(
+                        color: Colors.transparent,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Flexible(child: Text(''))
+        ],
+      ),
+    );
   }
 }
