7024d78602421ec6214f03731c434ee336f25fb8
anishtupe <tupeanish31@gmail.com>
2024-08-11 13:49:17 +0530

    made sign in page

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,11 +1,42 @@
 import 'package:cuisine_decider/widgets/custom_scaffold.dart';
+import 'package:cuisine_decider/widgets/reusablebutton.dart';
+import 'package:cuisine_decider/widgets/reusabletext.dart';
 import 'package:flutter/material.dart';
 
-class signinscreen extends StatelessWidget {
+class signinscreen extends StatefulWidget {
   const signinscreen({super.key});
 
   @override
+  State<signinscreen> createState() => _signinscreenState();
+}
+
+class _signinscreenState extends State<signinscreen> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return customscaffold(child: Text('sign in'));
+    return customscaffold(
+        child: SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(
+            20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+        child: Column(
+          children: <Widget>[
+            reuseabletextfield("Enter email id", Icons.person_outlined, false,
+                _emailTextController),
+            const SizedBox(
+              height: 30,
+            ),
+            reuseabletextfield("Enter Password", Icons.lock_outlined, true,
+                _passwordTextController),
+            const SizedBox(
+              height: 30,
+            ),
+            firebasebutton(context, "sign in", () {})
+          ],
+        ),
+      ),
+    ));
   }
 }
---
+++ lib/widgets/reusablebutton.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+Container firebasebutton(BuildContext context, String title, Function ontap) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 50,
+    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+    child: ElevatedButton(
+      onPressed: () {
+        ontap();
+      },
+      child: Text(
+        title,
+        style: const TextStyle(
+            color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
+      ),
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith((states) {
+            if (states.contains(MaterialState.pressed)) {
+              return Colors.black26;
+            }
+
+            return Colors.white;
+          }),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)))),
+    ),
+  );
+}
