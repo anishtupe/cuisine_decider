8cbaadc43152d1c4d15f20275bfde6410966d613
anishtupe <tupeanish31@gmail.com>
2024-08-11 22:57:35 +0530

    edited title in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter auth',
+      title: 'cuisine decider',
       theme: ThemeData(),
       home: welcomescreen(),
     );
