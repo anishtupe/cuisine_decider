b7ba7b163214a7dd65d1798d454844a431030d95
anishtupe <tupeanish31@gmail.com>
2024-08-13 13:05:05 +0530

    changed welcome buttons

--- lib/widgets/welcome_button.dart
+++ lib/widgets/welcome_button.dart
@@ -12,14 +12,22 @@ class NavigationButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return TextButton(
       onPressed: () {
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => destinationPage),
         );
       },
-      child: Text(buttonText),
+      child: Text(
+        buttonText,
+        style: const TextStyle(
+          color: Color.fromARGB(
+              116, 0, 0, 0), // You can change the color to your preference
+          fontSize: 25.0, // Adjust the font size as needed
+          decoration: TextDecoration.none, // No underline by default
+        ),
+      ),
     );
   }
 }
