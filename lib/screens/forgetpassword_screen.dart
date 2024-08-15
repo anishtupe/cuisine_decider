0e894e7b6cc3bc8b1b3110c530e0499950e837d9
anishtupe <tupeanish31@gmail.com>
2024-08-15 22:41:26 +0530

    added forget password functionality

--- lib/screens/forgetpassword_screen.dart
+++ lib/screens/forgetpassword_screen.dart
@@ -1,5 +1,8 @@
 import 'package:cuisine_decider/widgets/custom_scaffold.dart';
+import 'package:cuisine_decider/widgets/reusablebutton.dart';
+import 'package:cuisine_decider/widgets/reusabletext.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class forgetpassword_screen extends StatefulWidget {
   const forgetpassword_screen({super.key});
@@ -9,8 +12,36 @@ class forgetpassword_screen extends StatefulWidget {
 }
 
 class _forgetpassword_screenState extends State<forgetpassword_screen> {
+  TextEditingController _emailTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const customscaffold(child: Text('forgot password'));
+    return customscaffold(
+      child: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(20, 12, 20, 0),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 20,
+              ),
+              reuseabletextfield("Enter Email Id", Icons.person_outlined, false,
+                  _emailTextController),
+              const SizedBox(
+                height: 20,
+              ),
+              firebasebutton(context, "Reset Password", () {
+                FirebaseAuth.instance
+                    .sendPasswordResetEmail(email: _emailTextController.text)
+                    .then((value) => Navigator.of(context).pop());
+              })
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
